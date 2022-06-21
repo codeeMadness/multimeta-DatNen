a0155cbfd668c016cf2071ae25ab1f40a1c990f3
codeeMadness <codee.madness@gmail.com>
2022-06-21 21:41:42 +0700

    - add touch controls

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ let virtualEnvironment = new VirtualEnvironment();
 // then fill your world with the stuff you want
 init();
 function init() {
-    virtualEnvironment.loadTerrain('//asset.airclass.io/public/terrains/DatNen.glb', 0, 0, 0, "glb", 40)
+    virtualEnvironment.loadTerrain('//asset.airclass.io/public/terrains/DatNen.glb', 0, 0, 0, "glb", 40);
+    virtualEnvironment.enableTouchControls(true);
     virtualEnvironment.spawnPlayer({spawn: {x:0, y:70, z:0}})
 }
 
