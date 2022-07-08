264a545a582ffcb3b4c36b593e14cf7cb099f48d
Codee Madness <codee.madness@gmail.com>
2022-07-08 21:33:18 +0700

    - update core

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-// import { VirtualEnvironment } from 'https://codeemadness.github.io/spacetime-standard-kit/js/VirtualEnvironment.js';
-
-// start by creating a basic virtual environment
-// let virtualEnvironment = new VirtualEnvironment();
-
-import { spacetime } from "../lib/core.js";
-
 // start by creating a basic virtual environment
 
-let virtualEnvironment = new spacetime.VirtualEnvironment();
+let virtualEnvironment = new VirtualEnvironment();
 // then fill your world with the stuff you want
 init();
 function init() {
-    virtualEnvironment.loadTerrain('//asset.airclass.io/public/terrains/DatNen.glb', 0, 0, 0, "glb", 40);
-    virtualEnvironment.spawnPlayer({spawn: {x:0, y:70, z:0}})
+    virtualEnvironment.loadTerrain('//asset.airclass.io/public/terrains/DatNen.glb', 0, 0, 0, "glb", 1);
+    virtualEnvironment.spawnPlayer({spawn: {x:0, y:10, z:0}})
 }
 
 // then start the animation
